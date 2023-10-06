5b97a90ecdb1295fed14176f64226c16310e107f
Sarah.Oliveira <130112511+sarah1930@users.noreply.github.com>
2023-10-06 20:55:54 -0300

    Add files via upload

---
+++ src/ResultadoEscolar.java
@@ -0,0 +1,15 @@
+
+public class ResultadoEscolar {
+
+    public static void main(String[] args) {
+        
+         int nota = 10;
+        
+       if (nota >= 7)  {
+           System.out.println("Aprovado");
+        
+       } else {
+          System.out.println("Reprovado"); 
+       }  
+    }                      
+}
